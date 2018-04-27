3e21289d34051f1dea49bcd2f0bc7d14663162df
EloyAroni <34385571+EloyAroni@users.noreply.github.com>
2018-04-27 12:10:42 -0500

    Update Tesis Automatizar NPP.R

--- Tesis Automatizar NPP.R
+++ Tesis Automatizar NPP.R
@@ -1,153 +1,95 @@
 #
-####Cuantificación del número de embarcaciones totales (NPP-VIIRS)########
+####Cuantificación del número de embarcaciones totales (NPP-VIIRS)#########
+###########################################################################
 library(raster) 
-library(sp)
-library(rgdal)
 library(igraph)
-library(tiff)
-library(base)#Delete Inf and -Inf
-library(imager) # gradient similar to Filter Sobel 
-library(matrixcalc)# convertir 
-library(sp)
-library(reshape2)# MATRIX
-library(rasterVis)  # raster visualisation
+library(base)
+library(imager)
+library(matrixcalc)
+library(reshape2)
+library(rasterVis)
 library(maptools)
-#library(rgeos)
-
-
-inputo_NPP <- raster("C:/Users/earoni/Desktop/Prueba R Tesis/GDNBO-SVDNB_npp_d20130114_t0722469_e0728273_b06298_c20170503150230436532_noaa_ops.tif")
-inputo_NPP
-
-inputo_NPP <- reclassify(inputo_NPP, cbind(-9999.000000, NA)) # convert value "NA" to 0
-#values(inputo_NPP)[values(inputo_NPP) = -9999.000000] = NA
-inputo_NPP
-#1.- Log10 transformation#####################################################################################################
-inputo_NPP <- log10(inputo_NPP) # For each pixel of the image a log10 is applied over the radiance value
-inputo_NPP
-
+library(rgeos)
+####1.Estandarización######################################################
+inputo_NPP <- raster("C:/Tesis/GDNBO-SVDNB_npp_d20130114_t0722469.tif")
+inputo_NPP <- reclassify(inputo_NPP, cbind(-9999.000000, NA))
+inputo_NPP <- log10(inputo_NPP)       #valores de radiancia del cada pixel
 inputo_NPP[is.infinite(inputo_NPP)] <- NA 
-inputo_NPP
 
-#2.- Transformation [0-255]###################################################################################################
-max=maxValue(inputo_NPP) 
-max
+max=maxValue(inputo_NPP)         
 min=minValue(inputo_NPP)
-min
-
-slope=254/(max-min)
-slope
-
+slope=254/(max-min)                   #transformar a escala [0-255]
 intercept=1-(slope*min)
-intercept
-
 
 image_grey=inputo_NPP*slope+intercept
-image_grey
+####2.Umbral de detección##################################################
 
-max=maxValue(image_grey) 
-max
-min=minValue(image_grey)
-min
-#3.- Threshold detection######################################################################################################
+image_grey <- reclassify(image_grey, cbind(-NA, NA))
+image_grey1 <- as.matrix(image_grey)   
+image_grey1 = shift.right(image_grey,1)      
+image_grey1 = raster(image_grey1)     #(i+1)
+imagen_gradiente = overlay(image_grey,image_grey1, 
+                           fun=function(x,y){return(x-y)})
+umbralmax=maxValue(imagen_gradiente)         
+umbralmin=minValue(imagen_gradiente)
 
-image_grey[ image_grey[] < 100 ] <- NA # delete value low 100 because this pixel is the black oceans.
-image_grey <- reclassify(image_grey, cbind(-NA, NA)) # convert value "NA" to 0
+umbral = umbralmin - (umbralmax-umbralmin)/2  
+                                      #valor del umbral 
+image_umbral[ imagen_gradiente[] < umbral ] <- NA 
+                                      #eliminando valores<umbral  
+image_umbral
+#####3.Segmentación######################################################## 
+require(sp)
+require(rgdal)
+library(tiff)
 
-image_grey
-saveimage_grey=image_grey
-saveimage_grey<- writeRaster(image_grey, 'C:/Users/earoni/Desktop/borrar/Han Xu/image_grey.tif', overwrite=TRUE)
+imagen_seg1 <- clump(image_umbral)
+matriz <- as.matrix(imagen_seg1)
 
-image_grey=image_grey
+lapply(seq_len(imagen_seg1@data@max), function(x) {
+  inds <- which(matriz == x, arr.ind = TRUE)
+  nrow <- diff(range(inds[, "row"])) + 1
+  ncol <- diff(range(inds[, "col"])) + 1
+  matrix(1, ncol = ncol, nrow = nrow)
+})
 
+lista_emb<- hclust(dist(data.frame(rownames=rownames(matriz@data),
+              x=coordinates(matriz)[,1],
+              y=coordinates(matriz)[,2])), method="complete")
 
-#############################################################################################################################
+lista_emb <- SpatialPointsDataFrame(matrix(c(x,y), ncol=2),
+              data.frame(ID=seq(1:length(x))),
+              proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+imagen_seg1<-as.raster(matrix)
+embarcaciones1 <- write.csv(lista_emb,
+              'C:/Tesis/listaEmbarcaciones1.csv',overwrite=TRUE)
+####4.Validación###########################################################
+embarcaciones1 <- read.csv("C:/Tesis/listaEmbarcaciones1.csv")
+imput_dataAIS <-read.csv("C:/Tesis/embarccionesAIS.csv")
 
-#XY <- raster("C:/Users/earoni/Desktop/borrar/Han Xu/GDNBO.tif")
-XY <- image_grey
+regresion <- lm(embarcaciones1 ~ imput_dataAIS,data=embarcaciones1)
+imagen_seg1
 
-XY
-which.min(XY)
-which.max(XY)
-head(XY)
+####5.Re-Segmentación######################################################
 
+#Get a data.frame of labelled regions
+imagen_seg1 <- d.max %>% threshold("96%") %>% label %>% as.data.frame
+#Add scale indices
+imagen_seg1 <- mutate(imagen_seg1,index=as.data.frame(i.max)$value)
+regs <- dplyr::group_by(imagen_seg1,value) %>% 
+  dplyr::summarise(mx=mean(x),my=mean(y),scale.index=mean(index))
+p <- ggplot(as.data.frame(imagen_seg1),aes(x,y))
+     +geom_raster(aes(fill=value))
+     +geom_point(data=regs,aes(mx,my,size=scale.index),pch=2,col="red")
+p+scale_fill_gradient(low="black",high="white")
+     +scale_x_continuous(expand=c(0,0))
+     +scale_y_continuous(expand=c(0,0),trans=scales::reverse_trans())
 
-########### Extract lat/long values ##########################
-GEO=XY
-GEO <- reclassify(GEO, cbind(NA, 0)) # convert value "NA" to 0
-GEO= rasterToPoints( GEO )
-colnames(GEO) <- c("xx","yy","value") # changue name column
-GEO <- as.data.frame(GEO)
-head(GEO)
+centers <- ddply(imagen_seg2,.(value),summarise,mx=mean(x),my=mean(y))
+centers <- dplyr::group_by(imagen_seg2,value) %>% 
+           dplyr::summarise(mx=mean(x),my=mean(y))
+imagen_seg2 <- as.data.frame(imagen_seg1) %>% subset(value>0)
+embarcaciones2 <- write.csv(imagen_seg2,
+                            'C:/Tesis/listaEmbarcaciones2.csv',overwrite=TRUE)
 
-
-###### Move one position (i+1) / extract cell values #########
-A <- as.matrix(XY)   # move in matrix
-A = shift.right( A, 1 )  # (i+1)
-A <- raster(A)
-
-A <- reclassify(A, cbind(NA, 0)) # convert value "NA" to 0
-A <- rasterToPoints(A)
-A <- as.data.frame(A)
-head(A)
-
-########### Creating data.frame (lat/long/value) #############
-A<-data.frame(GEO$xx, GEO$yy, A$layer)
-colnames(A)<-c('x',"y","z")
-head(A)
-
-################ Create mask of raster #######################
-#sat<-raster("C:/Users/earoni/Desktop/borrar/Han Xu/GDNBO.tif")
-sat<- image_grey
-r<-raster(sat) 
-r<-setValues(r,0) # cell values egual = 0
-
-x <- rasterize(A[, 1:2], r, A[,3], fun=mean) # rasterize data frame
-head(x)
-
-x <- reclassify(x, cbind(0, NA)) # convert value "0" to NA
-
-x <- writeRaster(x, 'C:/Users/earoni/Desktop/borrar/Han Xu/image_grey1.tif', overwrite=TRUE)
-
-image_grey1=x
-
-###############################################################
-
-image_grey=image_grey # imagen gris (i)
-image_grey1=image_grey1  # imagen gris (i+1)
-
-##### similar filter Soble #################################### 
-
-spole <- raster("C:/Users/earoni/Desktop/Prueba R Tesis/gradiente.tif")
-
-#r1 = init(r, function(x) sample(5:6, x, replace = TRUE))
-#r1
-#r2 = init(r, function(x) sample(1:2, x, replace = TRUE))
-#r2
-#imagen_gradiente = overlay(r1, r2, fun=function(x,y){return(x-y)})
-#r3
-imagen_gradiente = overlay(image_grey,image_grey1, fun=function(x,y){return(x-y)})
-imagen_gradiente
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
- 
- 
- 
-
- 
+#######################################################
